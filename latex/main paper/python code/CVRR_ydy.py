7dfd193ea72548113fe0501d637de05f3114b810
ISIPINK <115187707+ISIPINK@users.noreply.github.com>
2023-03-14 21:05:39 +0100

    MP starting non-linearity

--- latex/main paper/python code/CVRR_ydy.py
+++ latex/main paper/python code/CVRR_ydy.py
@@ -1,10 +1,9 @@
 from random import random as U
 def Y(t):
     u = U()
-    if t > 1:
-        return 1 + t*(Y(u*t)+Y(u*t))/2
-    return 1 + (Y(u*t)+Y(u*t))/2 if U() < t else 1
+    if t > 1: return 1+t**2/2 + t*(Y(u*t)-u*t)
+    return 1 + t + t**2/2 + (Y(u*t)-1-u*t if U() < t else 0)
 def y(t, nsim):
     return sum(Y(t) for _ in range(nsim))/nsim
 print(f"y(1) approx {y(1,10**3)}")
-# y(1) approx 2.73747265625
+# y(1) approx 2.734827303480301
