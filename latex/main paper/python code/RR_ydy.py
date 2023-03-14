2ff5c87ec9dd484b984c741c2e40a2521b06755f
ISIPINK <115187707+ISIPINK@users.noreply.github.com>
2023-03-14 16:15:03 +0100

    MP mods CVs

---
+++ latex/main paper/python code/RR_ydy.py
@@ -0,0 +1,8 @@
+from random import random as U
+def Y(t):
+    if t>1: return 1 + t*Y(U()*t)
+    return 1 + Y(U()*t) if U() < t else 1
+def y(t, nsim):
+    return sum(Y(t) for _ in range(nsim))/nsim
+print(f"y(1) approx {y(1,10**3)}")
+# y(1) approx 2.698
