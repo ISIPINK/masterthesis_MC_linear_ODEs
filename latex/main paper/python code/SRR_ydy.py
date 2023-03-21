549ea24e211a2c848a0cf6a66ee8967245b5f8b8
ISIPINK <115187707+ISIPINK@users.noreply.github.com>
2023-03-21 10:40:46 +0100

    p5 test of trap

--- latex/main paper/python code/SRR_ydy.py
+++ latex/main paper/python code/SRR_ydy.py
@@ -1,8 +1,7 @@
 from random import random as U
 def Y(t):
     u = U()
-    if t > 1:
-        return 1 + t*(Y(u*t)+Y(u*t))/2
+    if t > 1: return 1 + t*(Y(u*t)+Y(u*t))/2
     return 1 + (Y(u*t)+Y(u*t))/2 if U() < t else 1
 def y(t, nsim): return sum(Y(t) for _ in range(nsim))/nsim
 print(f"y(1) approx {y(1,10**3)}")
