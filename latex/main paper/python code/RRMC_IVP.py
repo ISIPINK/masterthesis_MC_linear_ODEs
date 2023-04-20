385bba3fb28bdd598ff4c2bd6f17d5d6385869c6
ISIPINK <115187707+ISIPINK@users.noreply.github.com>
2023-04-20 19:50:19 +0200

    finished DRRMC

--- latex/main paper/python code/RRMC_IVP.py
+++ latex/main paper/python code/RRMC_IVP.py
@@ -1,7 +1,7 @@
 from random import random as U
-def Y_in(t, tn, yn, D):
+def Y_in(t, tn, yn, h):
     S = tn + U()*(t-tn)  # \sim Uniform(T,t)
-    return yn + D*Y_in(S, tn, yn, D) if U() < (t-tn)/D else yn
-def Y_out(tn, D): # D is out step size
-    TT = tn-D if tn-D > 0 else 0  
-    return Y_in(tn, TT, Y_out(TT, D), D) if tn > 0 else 1
+    return yn + h*Y_in(S, tn, yn, h) if U() < (t-tn)/h else yn
+def Y_out(tn, h): # h is out step size
+    TT = tn-h if tn-h > 0 else 0  
+    return Y_in(tn, TT, Y_out(TT, h), h) if tn > 0 else 1
--- latex/main paper/python code/nonlinear_CVRRMC.py
+++ latex/main paper/python code/nonlinear_CVRRMC.py
@@ -1,18 +1,18 @@
 from random import random as U
-def Y_in(t, tn, yn, dyn, D, l):
+def Y_in(t, tn, yn, dyn, h, l):
     sol = yn  # initial conditon
     sol += t**2-tn**2 - (t**5-tn**5)/5  # source
     sol += (t-tn)*yn**2  # 0 order 
     sol += 2*((t**2-tn**2)/2 - tn*(t-tn))*yn*dyn  # 1 order 
-    if U()*l < (t-tn)/D:
+    if U()*l < (t-tn)/h:
         S = tn + U()*(t-tn)  # \sim Uniform(T,t)
-        sol += l*D*(Y_in(S, tn, yn, dyn, D, l)*
-            Y_in(S, tn, yn, dyn, D, l) - yn**2-2*(S-tn)*yn*dyn)
+        sol += l*h*(Y_in(S, tn, yn, dyn, h, l)*
+            Y_in(S, tn, yn, dyn, h, l) - yn**2-2*(S-tn)*yn*dyn)
     return sol
-def Y_out(t, D, l):
+def Y_out(t, h, l):
     yn, tn = 0, 0
     while tn < t:
-        tt = tn+D if tn+D < t else t
+        tt = tn+h if tn+h < t else t
         dyn = yn**2 - tn**4+2*tn
         yn = Y_in(tt, tn, yn, dyn, tt-tn, l)
         tn = tt
