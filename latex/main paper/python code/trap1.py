b9fc9444f98426e5fa83c8dad2579789e536cf1a
ISIPINK <115187707+ISIPINK@users.noreply.github.com>
2023-05-16 22:02:48 +0200

    MP: reread and fix

--- latex/main paper/python code/trap1.py
+++ latex/main paper/python code/trap1.py
@@ -2,8 +2,8 @@ from random import random as U
 from math import exp
 import numpy as np
 def f(x): return exp(x)
-def trapezium(n): return sum((f(x)+f(x+1/n))/2
-    for x in np.arange(0, 1, 1/n))/n
+def trapezium(n): return sum((f(j/n)+f((j+1)/n))/2
+    for j in range(n))/n
 def MCtrapezium(n, l=100):
     sol = 0
     for j in range(n):
@@ -16,5 +16,5 @@ def exact(a, b): return exp(b)-exp(a)
 def error(s): return (s-exact(0, 1))/exact(0, 1)
 print(f"  error:{error(trapezium(10000))}")
 print(f"MCerror:{error(MCtrapezium(10000,100))}")
-# error:8.333344745642098e-10
-# MCerror:8.794793540941216e-11
+#   error:8.333344745642098e-10
+# MCerror:-1.5216231703870405e-10
