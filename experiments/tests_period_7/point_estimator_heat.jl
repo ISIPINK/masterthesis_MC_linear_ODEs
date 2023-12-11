e7410d6cb315a1ba8d79a03146fda552f3adcb88
isi <isidoor.pinilloesquivel@student.uantwerpen.be>
2023-12-11 18:28:17 +0100

    implented source point estimator

--- experiments/tests_period_7/point_estimator_heat.jl
+++ experiments/tests_period_7/point_estimator_heat.jl
@@ -23,6 +23,7 @@ y(x, t, Δx, nsim) = sum(Y(x, t, Δx) for _ in 1:nsim) / nsim
 ytail(x, t, Δx, nsim) = sum(Ytail(x, t, Δx) for _ in 1:nsim) / nsim
 
 u(x, t) = 2x * t + x^3 / 3 # needs to satisfy the heat equation
+# u(x, t) = exp(-t) * sin(x)
 x = 0.7
 t = 0.8
 nsim = 10^4
@@ -37,3 +38,29 @@ using BenchmarkTools
 
 
 
+function Ysource(x, t, Δx)
+    xold = x
+    tt = t + Δx^2 * log(rand()) / 2  #for the source term but can also be t
+    t += Δx^2 * log(rand()) / 2
+    x += rand(Bool) ? Δx : -Δx
+    return t < 0 ? u(xold, 0) :
+           (x < 0 ? u(0, t) :
+            x > 1 ? u(1, t) :
+            Ysource(x, t, Δx)) +
+           (rand() < Δx^2 / 2 ? f(xold, tt) : 0)
+end
+
+ysource(x, t, Δx, nsim) = sum(Ysource(x, t, Δx) for _ in 1:nsim) / nsim
+
+u(x, t) = x^2 * t^2 # needs to satisfy the heat equation
+f(x, t) = -2 * t^2 + 2 * x^2 * t # needs to satisfy the heat equation 
+x = 0.7
+t = 0.7
+nsim = 10^4
+Δx = 0.05
+println("error = $(ysource(x, t, Δx, nsim) - u(x, t))")
+
+using BenchmarkTools
+
+@benchmark y(x, t, Δx, nsim)
+@benchmark ytail(x, t, Δx, nsim)
