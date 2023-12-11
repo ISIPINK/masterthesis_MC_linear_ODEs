2bbdbec430fbe97f19bcae5aa68b5e938d60d6fd
isi <isidoor.pinilloesquivel@student.uantwerpen.be>
2023-12-11 09:59:01 +0100

    added julia code point estimator heat equation

--- experiments/tests_period_7/point_estimator_heat.jl
+++ experiments/tests_period_7/point_estimator_heat.jl
@@ -1,35 +1,39 @@
-@inline U() = rand()
-@inline B(p) = U() < p
-u(x, t) = 2x * t + x^3 / 3 # needs to satisfy the heat equation
-
-# recursive version
 function Y(x, t, Δx)
-    S = t + Δx^2 * log(U()) / 2
-    xnew = x + Δx * (2 * B(0.5) - 1)
-    return S < 0 ? u(x, 0) :
-           xnew < 0 ? u(0, S) :
-           xnew > 1 ? u(1, S) : Y(xnew, S, Δx)
+    xold = x
+    t += Δx^2 * log(rand()) / 2
+    x += rand(Bool) ? Δx : -Δx
+    return t < 0 ? u(xold, 0) :
+           x < 0 ? u(0, t) :
+           x > 1 ? u(1, t) : Y(x, t, Δx)
 end
 
-# u(x,0) here evaluated at xnew
-function Y(x, t, Δx)
+function Ytail(x, t, Δx)
     while true
-        t += Δx^2 * log(U()) / 2
-        x += Δx * (2 * B(0.5) - 1)
-        return t < 0 ? u(x, 0) :
+        xold = x
+        t += Δx^2 * log(rand()) / 2
+        x += rand(Bool) ? Δx : -Δx
+        return t < 0 ? u(xold, 0) :
                x < 0 ? u(0, t) :
                x > 1 ? u(1, t) : continue
     end
 end
 
-y(x, t, Δx, nsim) = sum(Y(x, t, Δx) for _ in 1:nsim) / nsim
 
+y(x, t, Δx, nsim) = sum(Y(x, t, Δx) for _ in 1:nsim) / nsim
+ytail(x, t, Δx, nsim) = sum(Ytail(x, t, Δx) for _ in 1:nsim) / nsim
+
+u(x, t) = 2x * t + x^3 / 3 # needs to satisfy the heat equation
 x = 0.7
 t = 0.8
 nsim = 10^4
 Δx = 0.05
 println("error = $(y(x, t, Δx, nsim) - u(x, t))")
+println("errortail = $(ytail(x, t, Δx, nsim) - u(x, t))")
 
+using BenchmarkTools
+
+@benchmark y(x, t, Δx, nsim)
+@benchmark ytail(x, t, Δx, nsim)
 
 
 
