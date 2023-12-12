e9bb0596adaccefcd3e15ad8c3b46e7fa4181388
isi <isidoor.pinilloesquivel@student.uantwerpen.be>
2023-12-12 20:01:18 +0100

    optimized point estimator for heat source

--- experiments/tests_period_7/point_estimator_heat.jl
+++ experiments/tests_period_7/point_estimator_heat.jl
@@ -38,58 +38,6 @@ using BenchmarkTools
 
 
 
-function Ysource(x, t, Δx)
-    xold = x
-    tt = t + Δx^2 * log(rand()) / 2  #for the source term but can also be t
-    t += Δx^2 * log(rand()) / 2
-    x += rand(Bool) ? Δx : -Δx
-    return t < 0 ? u(xold, 0) :
-           (x < 0 ? u(0, t) :
-            x > 1 ? u(1, t) :
-            Ysource(x, t, Δx)) +
-           (rand() < Δx^2 / 2 ? f(xold, tt) : 0)
-end
 
-using Distributions
 
-function Ysourcejump(x, t, Δx, sourcejump)
-    tmp = 0
-    xold = x
-    if sourcejump == -1
-        g = Geometric(Δx^2 / 2)
-        sourcejump = rand(g)
-    end
-    if sourcejump == 0
-        g = Geometric(Δx^2 / 2)
-        sourcejump = rand(g) + 1
-        tt = t + Δx^2 * log(rand()) / 2
-        tmp = f(xold, tt)
-    end
-    t += Δx^2 * log(rand()) / 2
-    x += rand(Bool) ? Δx : -Δx
-    return t < 0 ? u(xold, 0) :
-           (x < 0 ? u(0, t) :
-            x > 1 ? u(1, t) :
-            Ysourcejump(x, t, Δx, sourcejump - 1)) + tmp
-end
 
-ysource(x, t, Δx, nsim) = sum(Ysource(x, t, Δx) for _ in 1:nsim) / nsim
-ysourcejump(x, t, Δx, nsim) = sum(Ysourcejump(x, t, Δx, -1) for _ in 1:nsim) / nsim
-
-u(x, t) = x^2 * t^2 # needs to satisfy the heat equation
-f(x, t) = -2 * t^2 + 2 * x^2 * t # needs to satisfy the heat equation 
-x = 0.7
-t = 0.7
-nsim = 10^4
-Δx = 0.05
-println("error = $(ysource(x, t, Δx, nsim) - u(x, t))")
-println("error = $(ysourcejump(x, t, Δx, nsim) - u(x, t))")
-
-using Plots
-g = Geometric(Δx^2 / 2)
-histogram(rand(g, 10^5), normalize=:pdf)
-
-using BenchmarkTools
-
-@benchmark y(x, t, Δx, nsim)
-@benchmark ytail(x, t, Δx, nsim)
---
+++ experiments/tests_period_7/point_estimator_heatsource.jl
@@ -0,0 +1,103 @@
+using Distributions
+using Dates
+
+using BenchmarkTools
+using Profile
+using PProf
+
+function Ysource(x, t, Δx)
+    ee = Exponential(Δx^2 / 2)
+    sol = 0
+    xold = x
+    while true
+        xold = x
+        if rand() < Δx^2 / 2
+            tt = t - rand(ee)  #for the source term but can also be t
+            sol += f(xold, tt)
+        end
+        t -= rand(ee)
+        x += rand(Bool) ? Δx : -Δx
+        return t < 0 ? u(xold, 0) + sol :
+               x < 0 ? u(0, t) + sol :
+               x > 1 ? u(1, t) + sol : continue
+    end
+end
+
+
+function Ysourcejump(x, t, Δx, sourcejump)
+    tmp = 0
+    xold = x
+    if sourcejump == -1
+        g = Geometric(Δx^2 / 2)
+        sourcejump = rand(g)
+    end
+    if sourcejump == 0
+        g = Geometric(Δx^2 / 2)
+        sourcejump = rand(g) + 1
+        tt = t + Δx^2 * log(rand()) / 2
+        tmp = f(xold, tt)
+    end
+    t += Δx^2 * log(rand()) / 2
+    x += rand(Bool) ? Δx : -Δx
+    return t < 0 ? u(xold, 0) :
+           (x < 0 ? u(0, t) :
+            x > 1 ? u(1, t) :
+            Ysourcejump(x, t, Δx, sourcejump - 1)) + tmp
+end
+
+function Ysourcejumptail(x, t, Δx)
+    g = Geometric(Δx^2 / 2)
+    ee = Exponential(Δx^2 / 2)
+    sol = 0
+    xold = x
+    sourcejump = rand(g)
+    while true
+        xold = x
+        sourcejump -= 1
+        if sourcejump == 0
+            sourcejump = rand(g) + 1
+            tt = t - rand(ee)
+            sol += f(xold, tt)
+        end
+        t -= rand(ee)
+        x += rand(Bool) ? Δx : -Δx
+        return t < 0 ? u(xold, 0) + sol :
+               x < 0 ? u(0, t) + sol :
+               x > 1 ? u(1, t) + sol : continue
+    end
+
+end
+
+ysource(x, t, Δx, nsim) = sum(Ysource(x, t, Δx) for _ in 1:nsim) / nsim
+ysourcejump(x, t, Δx, nsim) = sum(Ysourcejump(x, t, Δx, -1) for _ in 1:nsim) / nsim
+ysourcejumptail(x, t, Δx, nsim) = sum(Ysourcejumptail(x, t, Δx) for _ in 1:nsim) / nsim
+function u(x, t)
+    # sleep(Microsecond(100))  #simulating expensive function call
+    return x^2 * t^2
+end
+
+function f(x, t)
+    # sleep(Microsecond(100))  #simulating expensive function call
+    return -2 * t^2 + 2 * x^2 * t
+end
+x = 0.7
+t = 0.7
+nsim = 10^4
+Δx = 0.05
+println("error = $(ysource(x, t, Δx, nsim) - u(x, t))")
+# println("error = $(ysourcejump(x, t, Δx, nsim) - u(x, t))")
+println("error = $(ysourcejumptail(x, t, Δx, nsim) - u(x, t))")
+
+
+
+@benchmark ysource(x, t, Δx, nsim)
+@benchmark ysourcejump(x, t, Δx, nsim)
+@benchmark ysourcejumptail(x, t, Δx, nsim)
+
+
+Profile.clear()
+@profile ysource(x, t, Δx, nsim)
+@profile ysourcejumptail(x, t, Δx, nsim)
+
+pprof()
+
