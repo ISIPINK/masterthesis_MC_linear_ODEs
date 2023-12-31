7d7c2ad70748cfc13e02bb3e10bf282eed2fa0db
isi <isidoor.pinilloesquivel@student.uantwerpen.be>
2023-12-31 12:19:40 +0100

    tested speed of YvarPath with a precomputed path

--- experiments/tests_period_7/pest_heat_varcoef.jl
+++ experiments/tests_period_7/pest_heat_varcoef.jl
@@ -66,6 +66,21 @@ function YvarPath(x, t, dx, a0, am, u_bound::Function, f::Function, a::Function)
     t >= eps() ? sol + w * u_bound(x, t) : sol + w * u_bound(x, 0)
 end
 
+function YvarPathMemo(x, t, dx, a0, am, u_bound::Function, f::Function, a::Function, path)
+    spoints, exit = path
+    (siginv = 1 / (2 / dx^2 + a0); p_source = am / (am + 2 / dx^2))
+    w_multiplier = 2 * siginv / (dx^2 * (1 - p_source))
+    (sol = 0; w = w_multiplier^spoints[1][3])
+    for (x, t, sterm_counter) in spoints[2:end]
+        sol += w * f(x, t) * siginv / p_source
+        w *= (a(x, t) + a0) * siginv / p_source
+        w *= w_multiplier^sterm_counter
+    end
+    (x, t, sterm_counter) = exit # small w can produce NaNs
+    w /= sterm_counter > 0 && abs(w) > eps() ? w_multiplier^sterm_counter : 1
+    t >= eps() ? sol + w * u_bound(x, t) : sol + w * u_bound(x, 0)
+end
+
 
 # following should holdl: ut = uxx + au +f  
 function u(x, t)
@@ -101,6 +116,11 @@ println("error = $(yvarpath(x, t, Δx, nsim,a0,am) - u(x, t))")
 
 @benchmark yvar(x, t, Δx, nsim, a0, am)
 @benchmark yvarpath(x, t, Δx, nsim, a0, am)
+
+path = genPath(x, t, Δx, a0, am)
+@benchmark Yvar(x, t, Δx, a0, am, u, f, a)
+@benchmark YvarPath(x, t, Δx, a0, am, u, f, a)
+@benchmark YvarPathMemo(x, t, Δx, a0, am, u, f, a, path)
 
 Profile.clear()
 @profile yvar(x, t, Δx, nsim, a0, am)
